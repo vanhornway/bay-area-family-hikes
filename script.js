1ff9e87483e5f400d33812e48ef1be164af7e532
vanhornway <mumair@gmail.com>
2026-03-13 00:06:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const hikes = {
 
 spring:[
@@ -7,31 +6,56 @@ spring:[
 name:"Edgewood Trail Loop",
 distance:"3.3 mi",
 elevation:"587 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/edgewood-trail-loop",
-
 map:"https://maps.google.com/?q=Edgewood+Park+Redwood+City",
-
 food:[
 ["Falafel Tazah","https://maps.google.com/?q=Falafel+Tazah+Redwood+City"],
 ["Mazra Mediterranean","https://maps.google.com/?q=Mazra+Mediterranean"]
 ],
-
 activities:[
 ["Filoli Gardens","https://maps.google.com/?q=Filoli+Gardens"],
 ["Pulgas Water Temple","https://maps.google.com/?q=Pulgas+Water+Temple"]
 ]
-
 },
 
 {
 name:"Russian Ridge Loop",
 distance:"4 mi",
 elevation:"650 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/russian-ridge-open-space-preserve",
-
 map:"https://maps.google.com/?q=Russian+Ridge+Open+Space"
+},
+
+{
+name:"Wunderlich Alambique Loop",
+distance:"3.2 mi",
+elevation:"679 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/alambique-redwood-and-bear-gulch-loop",
+map:"https://maps.google.com/?q=Wunderlich+County+Park"
+},
+
+{
+name:"Arastradero Preserve Loop",
+distance:"4.4 mi",
+elevation:"597 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/ohlone-trail-to-arastradero-creek-trail",
+map:"https://maps.google.com/?q=Arastradero+Preserve"
+},
+
+{
+name:"Coyote Valley Arrowhead Loop",
+distance:"3.9 mi",
+elevation:"620 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/arrowhead-loop-trail--2",
+map:"https://maps.google.com/?q=Coyote+Valley+Open+Space+Preserve"
+},
+
+{
+name:"Foothills Preserve Chamise Loop",
+distance:"3.9 mi",
+elevation:"649 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/chamise-toyon-and-fern-loop",
+map:"https://maps.google.com/?q=Foothills+Nature+Preserve"
 }
 
 ],
@@ -42,31 +66,56 @@ summer:[
 name:"Fall Creek Lime Kilns",
 distance:"3.4 mi",
 elevation:"511 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/fall-creek-to-lime-kilns-trail",
-
 map:"https://maps.google.com/?q=Fall+Creek+Trail+Henry+Cowell",
-
 food:[
 ["Falafel of Santa Cruz","https://maps.google.com/?q=Falafel+Santa+Cruz"],
-["Zameen","https://maps.google.com/?q=Zameen+Mediterranean+Santa+Cruz"]
+["Zameen Mediterranean","https://maps.google.com/?q=Zameen+Mediterranean+Santa+Cruz"]
 ],
-
 activities:[
 ["Roaring Camp Railroad","https://maps.google.com/?q=Roaring+Camp+Railroad"],
 ["Santa Cruz Boardwalk","https://maps.google.com/?q=Santa+Cruz+Boardwalk"]
 ]
-
 },
 
 {
-name:"Nisene Marks – Aptos Creek",
+name:"Henry Cowell Redwood Grove Loop",
+distance:"4.4 mi",
+elevation:"682 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/redwood-grove-loop-trail-and-cowell-highlights-loop",
+map:"https://maps.google.com/?q=Henry+Cowell+Redwoods+State+Park"
+},
+
+{
+name:"Nisene Marks Aptos Creek Loop",
+distance:"4 mi",
+elevation:"603 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/monte-toyon-vista-loop-via-aptos-rancho-trail",
+map:"https://maps.google.com/?q=Forest+of+Nisene+Marks+State+Park"
+},
+
+{
+name:"Reinhardt Redwood Stream Trail Loop",
+distance:"3.9 mi",
+elevation:"554 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/redwood-regional-park--6",
+map:"https://maps.google.com/?q=Redwood+Regional+Park"
+},
+
+{
+name:"Long Ridge Loop",
+distance:"4.2 mi",
+elevation:"652 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/long-ridge-loop",
+map:"https://maps.google.com/?q=Long+Ridge+Open+Space"
+},
+
+{
+name:"Sanborn County Park Summit Rock Loop",
 distance:"4 mi",
 elevation:"600 ft",
-
-alltrails:"https://www.alltrails.com/trail/us/california/monte-toyon-vista-loop-via-aptos-rancho-trail",
-
-map:"https://maps.google.com/?q=Nisene+Marks+State+Park"
+alltrails:"https://www.alltrails.com/parks/us/california/sanborn-county-park",
+map:"https://maps.google.com/?q=Sanborn+County+Park"
 }
 
 ],
@@ -74,33 +123,58 @@ map:"https://maps.google.com/?q=Nisene+Marks+State+Park"
 fall:[
 
 {
-name:"Rancho San Antonio High Meadow",
+name:"Rancho San Antonio High Meadow Loop",
 distance:"4.6 mi",
 elevation:"616 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/high-meadow-loop-via-lower-meadow-trail",
-
 map:"https://maps.google.com/?q=Rancho+San+Antonio+Open+Space",
-
 food:[
 ["DishDash Sunnyvale","https://maps.google.com/?q=DishDash+Sunnyvale"],
 ["Zareens","https://maps.google.com/?q=Zareens+Palo+Alto"]
 ],
-
 activities:[
 ["Deer Hollow Farm","https://maps.google.com/?q=Deer+Hollow+Farm"]
 ]
-
 },
 
 {
-name:"Arastradero Preserve Loop",
-distance:"4.4 mi",
-elevation:"597 ft",
+name:"Saratoga Gap Achistaca Loop",
+distance:"4.5 mi",
+elevation:"636 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/saratoga-gap-trail-to-achistaca-trail-loop",
+map:"https://maps.google.com/?q=Saratoga+Gap+Open+Space"
+},
 
-alltrails:"https://www.alltrails.com/trail/us/california/ohlone-trail-to-arastradero-creek-trail",
+{
+name:"Los Gatos Creek to Jones Trail Loop",
+distance:"4 mi",
+elevation:"521 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/los-gatos-creek-trail-to-jones-trail-loop",
+map:"https://maps.google.com/?q=St+Josephs+Hill+Open+Space"
+},
 
-map:"https://maps.google.com/?q=Arastradero+Preserve"
+{
+name:"Almaden Quicksilver Hacienda Loop",
+distance:"3.6 mi",
+elevation:"692 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/hacienda-randol-and-buena-vista-trail-loop",
+map:"https://maps.google.com/?q=Almaden+Quicksilver+County+Park"
+},
+
+{
+name:"Frog Lake Loop Henry Coe",
+distance:"4.5 mi",
+elevation:"698 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/frog-lake-loop-trail",
+map:"https://maps.google.com/?q=Henry+W+Coe+State+Park"
+},
+
+{
+name:"Monte Bello Open Space Loop",
+distance:"4 mi",
+elevation:"650 ft",
+alltrails:"https://www.alltrails.com/parks/us/california/monte-bello-open-space-preserve",
+map:"https://maps.google.com/?q=Monte+Bello+Open+Space+Preserve"
 }
 
 ],
@@ -111,30 +185,55 @@ winter:[
 name:"Lands End Trail",
 distance:"3.4 mi",
 elevation:"547 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/lands-end-trail",
-
 map:"https://maps.google.com/?q=Lands+End+Trail",
-
 food:[
 ["Pakwan SF","https://maps.google.com/?q=Pakwan+San+Francisco"]
 ],
-
 activities:[
 ["Sutro Baths","https://maps.google.com/?q=Sutro+Baths"],
 ["Legion of Honor","https://maps.google.com/?q=Legion+of+Honor+Museum"]
 ]
-
 },
 
 {
-name:"Wildcat Gorge Loop – Tilden",
+name:"Wildcat Gorge Loop Tilden",
 distance:"3.4 mi",
 elevation:"511 ft",
-
 alltrails:"https://www.alltrails.com/trail/us/california/wildcat-gorge-meadows-canyon-and-curran-trail",
-
 map:"https://maps.google.com/?q=Tilden+Regional+Park"
+},
+
+{
+name:"Dunn Graham West Ridge Loop",
+distance:"3.7 mi",
+elevation:"580 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/dunn-graham-and-west-ridge-trail-loop",
+map:"https://maps.google.com/?q=Redwood+Regional+Park+Oakland"
+},
+
+{
+name:"Tennessee Valley Loop",
+distance:"3.4 mi",
+elevation:"550 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/tennessee-valley-trail",
+map:"https://maps.google.com/?q=Tennessee+Valley+Trailhead"
+},
+
+{
+name:"Mount Tam Cataract Falls Loop",
+distance:"4.3 mi",
+elevation:"670 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/cataract-falls-trail",
+map:"https://maps.google.com/?q=Cataract+Falls+Mount+Tam"
+},
+
+{
+name:"Point Lobos Cypress Grove Loop",
+distance:"3.5 mi",
+elevation:"500 ft",
+alltrails:"https://www.alltrails.com/trail/us/california/cypress-grove-trail",
+map:"https://maps.google.com/?q=Point+Lobos+State+Reserve"
 }
 
 ]
@@ -176,7 +275,6 @@ section.innerHTML+=`
 <div class="links">
 
 <a href="${h.alltrails}" target="_blank">AllTrails</a>
-
 <a href="${h.map}" target="_blank">Google Maps (Trailhead)</a>
 
 </div>
